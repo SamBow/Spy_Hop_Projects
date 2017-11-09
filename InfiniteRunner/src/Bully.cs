e5c3fdf499dd7b17b0f8fb82886841137a4ae806
Sam Cookson <sbcskyline18@gmail.com>
2017-11-08 22:53:38 -0700

    Update Bully.cs

--- InfiniteRunner/src/Bully.cs
+++ InfiniteRunner/src/Bully.cs
@@ -6,17 +6,9 @@ public class Bully : MonoBehaviour {
 	private float moveSpeed = 12f;
 
 	private Rigidbody2D rigid;
-
-	void Start () {
-		//rigid = GetComponent<Rigidbody2D>();
-	}
 	
 	void Update () {
 
 		gameObject.transform.position = new Vector2(transform.position.x + .20f, transform.position.y);
-	}
-
-	void OnCollisionEnter2D(Collision2D collision) {
-
 	}
 }
