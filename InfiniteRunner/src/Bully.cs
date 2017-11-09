4c25cc21e57ddd65759c8739da0231adadbd6e0a
Sam Cookson <sbcskyline18@gmail.com>
2017-11-08 22:53:21 -0700

    Update Bully.cs

--- InfiniteRunner/src/Bully.cs
+++ InfiniteRunner/src/Bully.cs
@@ -14,10 +14,6 @@ public class Bully : MonoBehaviour {
 	void Update () {
 
 		gameObject.transform.position = new Vector2(transform.position.x + .20f, transform.position.y);
-		//gameObject.transform.Translate(Vector2.right * 0.25f);
-		//transform.position = new Vector3(transform.position.x, player.position.y, moveSpeed * Time.time);
-		//rigid.velocity = new Vector2(moveSpeed, rigid.velocity.y);
-		//bully.transform.position = new Vector2(bully.transform.position.x, player.transform.position.y);
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
