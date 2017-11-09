aac919ccc5b5aba467c2e56edd75da1c2ccb52eb
Sam Cookson <sbcskyline18@gmail.com>
2017-11-08 23:27:57 -0700

    Update launcher.js

--- Submarine/src/launcher.js
+++ Submarine/src/launcher.js
@@ -1,6 +1,3 @@
-// launch code
-// put on launcher that should be a child of the camera
-
 
 static var speed = 100;
 
@@ -9,18 +6,12 @@ var projectile: Rigidbody;
 
 static var magicone = true;
 
-
-//var shootsound1: AudioClip;
-//var shootsound2: AudioClip;
-//var shootsound3: AudioClip;
-
 function Update () {
 	
 	
 	
-		if(Input.GetButtonDown("Fire1"))
+	if(Input.GetButtonDown("Fire1"))
 	{
-		//audio.PlayOneShot(shootsound1);
 		var instantiatedProjectile: Rigidbody = Instantiate(projectile, transform.position, transform.rotation);
 		instantiatedProjectile.velocity = transform.TransformDirection(Vector3(0,0,speed));
 		Physics.IgnoreCollision(instantiatedProjectile.collider, transform.root.collider);
